a5c952d7b5f09cd247a798907dffa7c80f8f15da
a993096281 <993096281@qq.com>
2019-09-05 14:52:13 +0800

    modify nvmskiplist test

--- test_sh/nvmskiplist_test.sh
+++ test_sh/nvmskiplist_test.sh
@@ -4,8 +4,12 @@ workload="workloads/workloada.spec"
 dbpath="/home/lzw/ceshi2"
 moreworkloads="workloads/workloada.spec:workloads/workloadb.spec:workloads/workloadc.spec:workloads/workloadd.spec:workloads/workloade.spec:workloads/workloadf.spec"
 
-./ycsbc -db nvmskiplist -dbpath $dbpath -threads 1 -P $workload -load true -run true -dbstatistics true
+#./ycsbc -db nvmskiplist -dbpath $dbpath -threads 1 -P $workload -load true -run true -dbstatistics true
 
 #./ycsbc -db nvmskiplist -dbpath $dbpath -threads 1 -P $workload -load true -morerun $moreworkloads -dbstatistics true
 
 #./ycsbc -db nvmskiplist -dbpath $dbpath -threads 1 -P $workload -load true -morerun workloads/workloade.spec -dbstatistics true
+
+cmd="./ycsbc -db nvmskiplist -dbpath $dbpath -threads 1 -P $workload -load true -morerun $moreworkloads -dbstatistics true "
+echo $cmd
+eval $cmd
