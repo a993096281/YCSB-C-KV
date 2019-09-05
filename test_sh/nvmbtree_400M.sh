844d28e2ecf7eb8b11a7506aa268a17c6428bc93
a993096281 <993096281@qq.com>
2019-09-05 10:45:48 +0800

    modify test

--- test_sh/nvmbtree_400M.sh
+++ test_sh/nvmbtree_400M.sh
@@ -4,12 +4,12 @@ workload="workloads/400M_load.spec"
 dbpath="/home/lzw/ceshi2"
 moreworkloads="workloads/50M_workloada.spec:workloads/50M_workloadb.spec:workloads/50M_workloadc.spec:workloads/50M_workloadd.spec:workloads/1M_workloade.spec:workloads/50M_workloadf.spec"
 
-#./ycsbc -db scaledkv -dbpath $dbpath -threads 1 -P $workload -load true -run true -dbstatistics true
+#./ycsbc -db nvmbtree -dbpath $dbpath -threads 1 -P $workload -load true -run true -dbstatistics true
 
-#./ycsbc -db scaledkv -dbpath $dbpath -threads 1 -P $workload -load true -morerun $moreworkloads -dbstatistics true
+#./ycsbc -db nvmbtree -dbpath $dbpath -threads 1 -P $workload -load true -morerun $moreworkloads -dbstatistics true
 
 
-cmd="./ycsbc -db scaledkv -dbpath $dbpath -threads 1 -P $workload -load true -morerun $moreworkloads -dbstatistics true >>out.out 2>&1 "
+cmd="./ycsbc -db nvmbtree -dbpath $dbpath -threads 1 -P $workload -load true -morerun $moreworkloads -dbstatistics true >>out.out 2>&1 "
 echo $cmd >out.out
 echo $cmd
 eval $cmd
